722bdda16fcf56295238d458e2681f2091e36f09
sangpok <48979587+sangpok@users.noreply.github.com>
2022-11-04 09:43:07 +0900

    feat: Create the method of play()

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+const { Random, Console } = require('@woowacourse/mission-utils');
+
 class App {
-  play() {}
+    constructor() {}
+    play() {
+        Console.print('숫자 야구 게임을 시작합니다.');
+        this.pickComputerNumber();
+        this.startBaseballGame();
+    }
 }
 
 module.exports = App;
