5db67fa476ea7158844fec4cb7ddca64e8be7f5e
sangpok <48979587+sangpok@users.noreply.github.com>
2022-11-04 09:47:39 +0900

    feat: Create the method of pickComputerNumber()
    the computer would pick up the three number

--- src/App.js
+++ src/App.js
@@ -2,6 +2,19 @@ const { Random, Console } = require('@woowacourse/mission-utils');
 
 class App {
     constructor() {}
+
+    pickComputerNumber() {
+        let randomNumber = [];
+        while (randomNumber.length < 3) {
+            const number = Random.pickNumberInRange(1, 9);
+            if (!randomNumber.includes(number)) {
+                randomNumber.push(number);
+            }
+        }
+
+        this._computerNumber = randomNumber;
+    }
+
     play() {
         Console.print('숫자 야구 게임을 시작합니다.');
         this.pickComputerNumber();
