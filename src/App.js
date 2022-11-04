7392761dca625ff61ec6b1bbd5607c0dcfecc1d9
sangpok <48979587+sangpok@users.noreply.github.com>
2022-11-04 09:58:17 +0900

    feat: Create the method of returnHint()
    it returns the hint about inputed number

--- src/App.js
+++ src/App.js
@@ -15,6 +15,30 @@ class App {
         this._computerNumber = randomNumber;
     }
 
+    returnHint(userNumbers) {
+        const [strikeCount, ballCount] = [
+            this.countStrike(userNumbers),
+            this.countBall(userNumbers),
+        ];
+
+        console.log('computeNumber: ' + this._computerNumber);
+        console.log('strikeCount: ' + strikeCount);
+        console.log('ballCount: ' + ballCount);
+    }
+
+    countStrike(userNumbers) {
+        return userNumbers.filter((userNumber, index) => userNumber === this._computerNumber[index])
+            .length;
+    }
+
+    countBall(userNumbers) {
+        return userNumbers.filter(
+            (userNumber, index) =>
+                this._computerNumber.includes(userNumber) &&
+                this._computerNumber[index] !== userNumber
+        );
+    }
+
     play() {
         Console.print('숫자 야구 게임을 시작합니다.');
         this.pickComputerNumber();
