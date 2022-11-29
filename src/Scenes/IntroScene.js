a3effe5851333df9729b64763a8fffe08b8bb614
sangpok <48979587+sangpok@users.noreply.github.com>
2022-11-29 10:06:12 +0900

    chore: Jsdoc 주석 추가

--- src/Scenes/IntroScene.js
+++ src/Scenes/IntroScene.js
@@ -1,5 +1,10 @@
+/** 게임코어 import */
 const GameCore = require("../Domains/GameCore");
+
+/** View Import */
 const OutputView = require("../Views/OutputView");
+
+/** 상수 import */
 const { SCENE } = require("../Constants");
 
 const IntroScene = {
