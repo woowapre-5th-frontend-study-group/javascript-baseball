99e8f151cae71197a5ded80a0a518d4cb08800e2
sangpok <48979587+sangpok@users.noreply.github.com>
2022-11-29 10:08:18 +0900

    refactor: 상수화 및 불필요한 로직 제거

--- src/Utils/Validation.js
+++ src/Utils/Validation.js
@@ -1,5 +1,9 @@
+/** Validators import */
 const ValidatorGameCommand = require("./ValidatorGameCommand");
 const ValidatorNumber = require("./ValidatorNumber");
+
+/** 상수 import */
+const { ERROR, GAME_COMMAND } = require("../Constants");
 
 /** 각 상황에 맞는 조건을 생성하고 검사하는 역할 */
 const Validation = {
@@ -8,24 +12,17 @@ const Validation = {
    *
    * @param {string} value 입력받은 추측 숫자
    * @throws 앞선 조건들에 만족하지 않으면 throw를 발생
-   * @returns {boolean} 유효성 검사 통과 여부
    */
   validateNumber(value) {
-    try {
-      new ValidatorNumber(value)
-        .shouldBeNotEmpty()
-        .withMessage("빈 값이 아닌 다른 값을 입력해주세요.")
-        .shouldBeThree()
-        .withMessage("3자리의 서로 다른 숫자를 입력해주세요.")
-        .shouldBeNumberic()
-        .withMessage("문자가 아닌 숫자를 입력해주세요.")
-        .shouldBeExclusive()
-        .withMessage("서로 다른 3자리 숫자를 입력해주세요.");
-    } catch (error) {
-      throw error;
-    }
-
-    return true;
+    new ValidatorNumber(value)
+      .shouldBeNotEmpty()
+      .withMessage(ERROR.NOT_EMPTY)
+      .shouldBeThree()
+      .withMessage(ERROR.NOT_THREE)
+      .shouldBeNumberic()
+      .withMessage(ERROR.NOT_NUMBERIC)
+      .shouldBeExclusive()
+      .withMessage(ERROR.NOT_EXCLUSIVE);
   },
 
   /**
@@ -33,22 +30,15 @@ const Validation = {
    *
    * @param {string} value 입력받은 게임 커맨드
    * @throws 앞선 조건들에 만족하지 않으면 throw를 발생
-   * @returns {boolean} 유효성 검사 통과 여부
    */
   validateGameCommand(value) {
-    try {
-      new ValidatorGameCommand(value)
-        .shouldBeNotEmpty()
-        .withMessage("빈 값이 아닌 다른 값을 입력해주세요.")
-        .shouldBeOneInput()
-        .withMessage("1(새로 시작), 또는 2(종료)의 값을 하나만 입력해주세요.")
-        .shouldBeOne("1", "2")
-        .withMessage("1(새로 시작), 또는 2(종료)의 값만 입력해주세요.");
-    } catch (error) {
-      throw error;
-    }
-
-    return true;
+    new ValidatorGameCommand(value)
+      .shouldBeNotEmpty()
+      .withMessage(ERROR.NOT_EMPTY)
+      .shouldBeOneInput()
+      .withMessage(ERROR.NOT_ONE_INPUT)
+      .shouldBeOne(GAME_COMMAND.NEW_GAME, GAME_COMMAND.EXIT)
+      .withMessage(ERROR.NOT_ONE);
   },
 };
 
