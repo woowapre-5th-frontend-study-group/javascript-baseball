4cac42ce85756c5005f6ab1b1f985a60370287cb
sangpok <48979587+sangpok@users.noreply.github.com>
2022-11-29 10:02:53 +0900

    feat: Error 메시지 추가

--- src/Constants/index.js
+++ src/Constants/index.js
@@ -32,6 +32,12 @@ const Constants = {
 
   ERROR: {
     HEADING: "[ERROR]",
+    NOT_EMPTY: "빈 값이 아닌 다른 값을 입력해주세요.",
+    NOT_THREE: "3자리의 서로 다른 숫자를 입력해주세요.",
+    NOT_NUMBERIC: "문자가 아닌 숫자를 입력해주세요.",
+    NOT_EXCLUSIVE: "서로 다른 3자리 숫자를 입력해주세요.",
+    NOT_ONE_INPUT: "1(새로 시작), 또는 2(종료)의 값을 하나만 입력해주세요.",
+    NOT_ONE: "1(새로 시작), 또는 2(종료)의 값만 입력해주세요.",
   },
 
   INPUT_TYPE: {
