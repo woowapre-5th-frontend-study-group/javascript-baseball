f4c4e1049c4bd5f5db40d421eec656d85f538e23
sangpok <48979587+sangpok@users.noreply.github.com>
2022-11-29 10:03:33 +0900

    chore: Jsdoc 주석 추가

--- src/Domains/GameState.js
+++ src/Domains/GameState.js
@@ -6,12 +6,19 @@ let _answerGong = null;
 /** @type {Gong} */
 let _userGong = null;
 
+/** 게임 상태를 관리하는 객체 */
 const GameState = {
+  /**
+   * 정답 숫자를 설정한다.
+   *
+   * @param {Array<number>} numbers 정답 숫자
+   */
   setAnswerGong(numbers) {
     _answerGong = new Gong(numbers);
   },
 
   /**
+   * 정답 숫자를 반환한다.
    *
    * @returns {Gong}
    */
@@ -19,11 +26,17 @@ const GameState = {
     return _answerGong;
   },
 
+  /**
+   * 유저의 추측 숫자를 설정한다.
+   *
+   * @param {Array<number>} numbers
+   */
   setUserGong(numbers) {
     _userGong = new Gong(numbers);
   },
 
   /**
+   * 유저의 추측 숫자를 반환한다.
    *
    * @returns {Gong}
    */
@@ -31,6 +44,11 @@ const GameState = {
     return _userGong;
   },
 
+  /**
+   * 정답 숫자와 추측 숫자의 비교 결과를 반환한다.
+   *
+   * @returns {{ballCount: number, strikeCount: number}}
+   */
   getCompareResult() {
     return _userGong.compare(_answerGong);
   },
