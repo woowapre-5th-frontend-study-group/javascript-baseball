4319e586a82d5755b0c4f4d2d16417e2b9c560c9
Mcdonalds Trump <whdmschd2@gmail.com>
2022-11-30 20:12:53 +0900

    refactor: printStrikeBallCountMessage 추가 & strike ball 상수 수정

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -9,20 +9,13 @@ const OUTPUT_MESSAGE = {
   start: "숫자 야구 게임을 시작합니다.",
   end: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
   nothing: "낫싱",
-  countResult: ({ ball, strike }) =>
-    `${makeStrikeBallCountMessage.ball(
-      ball
-    )} ${makeStrikeBallCountMessage.strike(strike)}`,
+  strike: "스트라이크",
+  ball: "볼",
 };
 
 const ERROR_MESSAGE = {
   playerInput: "1부터 9까지 서로 다른 수로 이루어진 3자리의 수를 입력하세요",
   restart: "1과 2중 올바른 숫자를 입력하세요",
-};
-
-const makeStrikeBallCountMessage = {
-  strike: (count) => (count ? `${count}스트라이크` : ""),
-  ball: (count) => (count ? `${count}볼` : ""),
 };
 
 const exportsMessages = {
--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -10,11 +10,19 @@ const OutputView = {
     if (this.isNothing({ ball, strike })) {
       return Console.print(OUTPUT_MESSAGE.nothing);
     }
-    Console.print(OUTPUT_MESSAGE.countResult({ ball, strike }));
+
+    this.printStrikeBallCountMessage({ ball, strike });
   },
 
   printGameSuccess() {
     Console.print(OUTPUT_MESSAGE.end);
+  },
+
+  printStrikeBallCountMessage({ ball, strike }) {
+    const ballMessage = ball ? `${ball}${OUTPUT_MESSAGE.ball}` : "";
+    const strikeMessage = strike ? `${strike}${OUTPUT_MESSAGE.strike}` : "";
+
+    Console.print(`${ballMessage} ${strikeMessage}`);
   },
 
   isNothing({ ball, strike }) {
